ddcc4fc50e64997af65c3a1b8ea427a0d2f5b6ca
xue xinglong <chaosxinglong@gmail.com>
2026-05-27 11:41:10 +0800

    fix: HorseSpur and QuickSelector with precise Figma SVGs

--- src/components/hud/HorseSpur/HorseSpur.tsx
+++ src/components/hud/HorseSpur/HorseSpur.tsx
@@ -1,58 +1,29 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './horseSpur.module.less'
+import horseSpurNormalSvg from '../../../assets/svg/horse-spur-normal.svg'
 
 export type HorseSpurVariant = 'normal' | 'ancient' | 'endura'
 
 export interface HorseSpurProps {
-  /** 马刺变体 */
-  variant?: HorseSpurVariant
+  /** 冲刺类型 */
+  type?: HorseSpurVariant
   /** 是否已使用 */
   used?: boolean
-  /** 尺寸 */
+  /** 尺寸（默认 84px） */
   size?: number
-  /** 自定义类名 */
   className?: string
-  /** 自定义样式 */
   style?: React.CSSProperties
 }
 
-const HorseSpur: React.FC<HorseSpurProps> = ({
-  variant = 'normal',
-  used = false,
-  size = 28,
-  className,
-  style,
-}) => {
-  const cls = classNames(
-    styles.spur,
-    styles[variant],
-    { [styles.used]: used },
-    className
-  )
-
-  return (
-    <div className={cls} style={{ width: size, height: size, ...style }}>
-      <svg
-        className={styles.icon}
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12 2L14.5 8.5L21 9.5L16 14L17.5 21L12 17.5L6.5 21L8 14L3 9.5L9.5 8.5L12 2Z"
-          fill="currentColor"
-          fillOpacity={used ? 0.3 : 1}
-        />
-        <path
-          d="M12 5L13.8 9.5L18.5 10.2L15 13.5L15.9 18.2L12 16L8.1 18.2L9 13.5L5.5 10.2L10.2 9.5L12 5Z"
-          fill="currentColor"
-          fillOpacity={used ? 0.15 : 0.6}
-        />
-      </svg>
-    </div>
-  )
-}
+const HorseSpur: React.FC<HorseSpurProps> = ({ type = 'normal', used = false, size = 84, className, style }) => (
+  <div
+    className={classNames(styles.container, styles[type], { [styles.used]: used }, className)}
+    style={{ width: size, height: size, ...style }}
+  >
+    <img src={horseSpurNormalSvg} alt="" className={styles.icon} />
+  </div>
+)
 
 HorseSpur.displayName = 'HorseSpur'
 export default HorseSpur
